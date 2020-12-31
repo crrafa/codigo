75e2f9c817bfe1807fcc08452b48ceb131c2c320
rafael <rafael.chaidez.r@gmail.com>
2020-12-31 14:56:28 -0700

    Este es el primer commit de esta practica

---
+++ numero.py
@@ -0,0 +1,14 @@
+def mayor(x,y):
+    if x>y:
+        return x
+    return y
+
+a=17
+b=18
+c=14
+d=25
+
+#esta funcion utilizar recursividad
+maximo = mayor(mayor(a,b), mayor(c,d))
+print (maximo)
+    
